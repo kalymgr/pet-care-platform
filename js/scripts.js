047dfc56ca86476c5a1deff52fbe3bb0680d80f5
Michalis <mtsougranis@gmail.com>
2020-01-03 15:45:23 +0200

    Finished with first iteration for Pet adoption features

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * Created by kalymgr on 3/1/2020.
+ */
+
+
+$(document).ready(function(){
+
+    /**
+     * Actions happening when selecting shelter or petowner radio boxes
+     */
+    $("#shelterOrPetOwner").click(function(){
+        // if shelter is selected, enable the form field about the shelter name, else disable
+        //TODO: complete the function content
+
+    })
+})

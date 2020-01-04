a4e993c763783a2d2b69d21262852fef91c924ab
Michalis <mtsougranis@gmail.com>
2020-01-04 09:37:57 +0200

    Changes on the home page - Added blockquote - Added carousel

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,35 @@ $(document).ready(function(){
         // if shelter is selected, enable the form field about the shelter name, else disable
         //TODO: complete the function content
 
+    });
+
+
+    /**
+     * Code for the home page carousel button
+     */
+    // start the carousel, setting each slide duration
+    $("#homePageCarousel").carousel({
+        interval: 5000
+    });
+
+    // set the carousel button functionality
+    $("#carouselButton").click(function() {
+        if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+            // Case the carousel button shows the pause icon.
+            // Pause the carousel and change the icon
+            $("#homePageCarousel").carousel("pause");
+            $("#carouselButton").children("span").removeClass("fa-pause");
+            $("#carouselButton").children("span").addClass("fa-play");
+        }
+        else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+            // Case the carousel button shows the play icon
+            // Play the carousel and change the icon
+            $("#homePageCarousel").carousel("cycle");
+            $("#carouselButton").children("span").removeClass("fa-play");
+            $("#carouselButton").children("span").addClass("fa-pause");
+        }
     })
+
+
+
 })
